5292194f9ff298e16842072a879c7b1a6cabf8e6
Bruno Assis <brunoassis@gmail.com>
2015-08-31 12:23:56 -0300

    Defined DefinitionCommand

--- lib/IntergalacticCalculator/commands/definition_command.rb
+++ lib/IntergalacticCalculator/commands/definition_command.rb
@@ -1,3 +1,13 @@
 class DefinitionCommand
   include Command
+
+  def initialize(alien_numeral, roman_numeral)
+    @alien_numeral = alien_numeral
+    @roman_numeral = roman_numeral
+    set_code
+  end
+
+  def set_code
+    @code = "#{@alien_numeral} = \"@roman_numeral\""
+  end
 end
