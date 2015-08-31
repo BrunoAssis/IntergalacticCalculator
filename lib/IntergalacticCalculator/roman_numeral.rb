c193ca30d98efbb4bde8f54e25a72e06becf4e70
Bruno Assis <brunoassis@gmail.com>
2015-08-31 16:59:53 -0300

    Fixed RomanNumeral class

--- lib/IntergalacticCalculator/roman_numeral.rb
+++ lib/IntergalacticCalculator/roman_numeral.rb
@@ -1,4 +1,20 @@
 class RomanNumeral
+  MAPPING = {
+    "M"   => 1000,
+    "CM"  => 900,
+    "D"   => 500,
+    "CD"  => 400,
+    "C"   => 100,
+    "XC"  => 90,
+    "L"   => 50,
+    "XL"  => 40,
+    "X"   => 10,
+    "IX"  => 9,
+    "V"   => 5,
+    "IV"  => 4,
+    "I"   => 1
+  }
+
   attr_reader :roman_numeral
 
   def initialize(roman_numeral)
@@ -6,6 +22,23 @@ class RomanNumeral
   end
 
   def to_arabic
-    1
+    if is_invalid? @roman_numeral
+      raise ArgumentError, "Wrong Roman Numeral"
+    else
+      result = 0
+      roman_string = @roman_numeral
+      MAPPING.each do |roman, value|
+        while roman_string.start_with?(roman)
+          result += value
+          roman_string.slice!(0, roman.length)
+        end
+      end
+      result
+    end
+  end
+
+  private
+  def is_invalid?(roman_numeral)
+    roman_numeral !~ /^(M{0,4})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/
   end
 end
